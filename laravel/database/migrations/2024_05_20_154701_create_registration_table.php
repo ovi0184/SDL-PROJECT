6c8ddbf040e8e2bc8676b22d8a86c958d4f4fe6b
Ovi Das <ovidas34@gmail.com>
2024-05-20 22:29:54 +0600

    file added

---
+++ laravel/database/migrations/2024_05_20_154701_create_registration_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registration', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->string('user_name',60);
+            $table->string('password');
+            $table->string('first_name',60);
+            $table->string('last_name',60);
+            $table->string('email',100);
+            $table->text('address');
+            $table->date('dob')->nullable();
+            $table->date('registration_date');
+            $table->boolean('status')->default(1);
+            $table->date('last_login');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registration');
+    }
+};
