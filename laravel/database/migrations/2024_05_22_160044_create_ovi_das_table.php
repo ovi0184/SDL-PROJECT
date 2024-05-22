87a7e0edaafb57d847aa31c114df61dfdbf2b8ce
Ovi Das <ovidas34@gmail.com>
2024-05-23 00:18:57 +0600

    database file added

---
+++ laravel/database/migrations/2024_05_22_160044_create_ovi_das_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ovi_das', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name',60);
+            $table->string('last_name',60);
+            $table->string('email');
+            $table->integer('phone_number');
+            $table->string('address');
+            $table->string('profile_picture',300);
+            $table->string('Bio');
+            $table->string('status');
+            $table->string('additional_information');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ovi_das');
+    }
+};
